4e5d83b4cb9c719f890b40514b63a614ab313ab5
Meowthpxnk <meowthpxnk@gmail.com>
2023-02-22 21:56:35 +0300

    first commit

--- app/views/callBackDataTg.py
+++ app/views/callBackDataTg.py
@@ -6,9 +6,9 @@ from app import app, bot
 @app.route('/callback_data_tg', methods=["POST"])
 def callbackDataTelegram():
     data = request.json
+    bot.send_message(text="123", chat_id="5693374811")
     if request.headers.get('content-type') == 'application/json':
         update = telebot.types.Update.de_json(data)
-        bot.send_message(text="123", chat_id="5693374811")
         bot.process_new_updates([update])
         return {"ok":True}
     else:
