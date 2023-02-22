570c082a94656bf1b61e372b2250f49e5af54531
Meowthpxnk <meowthpxnk@gmail.com>
2023-02-22 21:07:06 +0300

    first commit

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ import os
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(host="192.168.1.80")
-        # app.run()
+        # app.run(host="192.168.1.80")
+        app.run()
