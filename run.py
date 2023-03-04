fa7f4d4e01c7647b84dcf9b7b0f9e184155c6fe5
Meowthpxnk <meowthpxnk@gmail.com>
2023-03-05 01:56:05 +0300

    first commit

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 from app import app, db, bot
+
+from app.models import MenuDishSecond
 import os
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+        # dish = MenuDishSecond(1, "title", 150, "portion", "ingredients")
+        # db.session.add(dish)
+        # db.session.commit()
         # app.run(host="192.168.1.80")
         bot.send_message(text="123", chat_id="5693374811")
         bot.set_webhook(url="https://api.arcadakms.ru/callback_data_tg")
