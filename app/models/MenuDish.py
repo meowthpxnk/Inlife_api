4f46e9b46645d77d58b375e4cb038f2622c8444b
Meowthpxnk <meowthpxnk@gmail.com>
2023-03-05 11:09:14 +0300

    first commit

--- app/models/MenuDish.py
+++ app/models/MenuDish.py
@@ -1,32 +1,31 @@
 from app import db
-
 class MenuDish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     title = db.Column(db.String)
-    price = db.Column(db.String)
-    portion = db.Column(db.String)
-    ingredients = db.Column(db.String)
+    # price = db.Column(db.String)
+    # portion = db.Column(db.String)
+    # ingredients = db.Column(db.String)
 
     dishes = db.relationship('MenuDishSecond', backref='MenuDish')
 
 
     category_id = db.Column(db.Integer, db.ForeignKey('menu_category.id'))
 
-    def __init__(self, category_id, title=None, price=None, portion=None, ingredients=None):
+    def __init__(self, category_id, title=None):
         self.title = title
-        self.price = price
-        self.portion = portion
-        self.ingredients = ingredients
+        # self.price = price
+        # self.portion = portion
+        # self.ingredients = ingredients
 
         self.category_id = category_id
 
     def info(self):
         return {
             "title": self.title,
-            "price": self.price,
-            "portion": self.portion,
-            "ingredients": self.ingredients,
+            # "price": self.price,
+            # "portion": self.portion,
+            # "ingredients": self.ingredients,
             "category_id": self.category_id,
         }
 
--- run.py
+++ run.py
@@ -7,6 +7,4 @@ import os
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        bot.send_message(text="123", chat_id="5693374811")
-        bot.set_webhook(url="https://api.arcadakms.ru/callback_data_tg")
         app.run()
