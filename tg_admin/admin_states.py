5a66e07a6c1b2169db7ff18531f8fd2d9a92650c
Meowthpxnk <meowthpxnk@gmail.com>
2023-03-06 16:40:26 +0300

    first commit

--- tg_admin/admin_states.py
+++ tg_admin/admin_states.py
@@ -33,11 +33,11 @@ class Step:
         # if self.message:
         #     self.admin.send_answer(self.message, reply_markup=self.reply_markup)
         if self.unique_message:
-            self.admin.send_answer(self.unique_message)
-            self.admin.send_answer(self.unique_message)
+            self.admin.send_answer(self.unique_message())
+            self.admin.send_answer(self.unique_message())
         if self.unique_message:
-            self.admin.send_answer(self.unique_message)
-            self.admin.send_answer(self.unique_message)
+            self.admin.send_answer(self.unique_message())
+            self.admin.send_answer(self.unique_message())
         ...
         
 
